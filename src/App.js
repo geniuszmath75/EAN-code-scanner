8edc8c12365d588899cd72afdedce610108cd976
geniuszmath75 <geniuszmath75@gmail.com>
2024-02-12 15:55:16 +0100

    Add fixes in app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Scanner from './components/Scanner';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Scan Barcode</h1>
-      <Scanner />
-    </div>
+      <div className="App">
+          <h1>Scan Barcode</h1>
+          <Scanner />
+      </div>
   );
 }
 
